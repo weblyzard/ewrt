bb6ea9478592293138a23a188428dd69880350ab
albert <albert@52f8a927-27e1-4e1c-b33e-807c988f9568>
2008-07-22 13:21:20 +0000

    first working version. code cleanup.

--- util/cache.py
+++ util/cache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+""" caches arbitrary objects """
+
 # (C)opyrights 2008 by Albert Weichselbraun <albert@weichselbraun.net>
 # 
 # This program is free software: you can redistribute it and/or modify
@@ -20,6 +22,8 @@ __copyright__ = "GPL"
 
 from os import makedirs
 from os.path import join, exists
+from operator import attrgetter
+import cPickle
 
 try:
     import hashutils
@@ -35,17 +39,20 @@ class Cache(object):
     cache_dir = ""
     cache_file_suffix = ""
 
-    def __init__(self, cache_dir="", cache_nesting_level=0, cache_file_suffix=""):
+    def __init__(self, cache_dir, cache_nesting_level=0, cache_file_suffix=""):
+        """ initializes the Cache object """
+
         self.cache_dir = cache_dir
         self.cache_file_suffix = cache_file_suffix
-        self.cache_nesting_level = 0
+        self.cache_nesting_level = cache_nesting_level
 
         self._cache_hit  = 0
         self._cache_miss = 0
 
     @staticmethod
     def getObjectId( obj_str ):
-        return HASH(str)
+        """ returns an identifier representing the object """
+        return HASH(obj_str).hexdigest()
         
 
     def _get_fname( self, obj_id ):
@@ -55,14 +62,14 @@ class Cache(object):
         """
         assert( len(obj_id) >= self.cache_nesting_level )
 
-        obj_dir = join( *(self.cache_dir + list( obj_id[:self.cache_nesting_level] )) )
+        obj_dir = join( *( [self.cache_dir] + list( obj_id[:self.cache_nesting_level] )) )
         if not exists(obj_dir):
             makedirs(obj_dir)
 
         return join(obj_dir, obj_id+self.cache_file_suffix)
     
     
-    def fetch(self, obj_id, fetch_function):
+    def fetch(self, obj_id, fetch_function, inherited=False):
         """ fetches the object with the given id, querying
              a) the cache and
              b) the fetch_function
@@ -75,7 +82,10 @@ class Cache(object):
             return open(cache_file).read()
         else:
             self._cache_miss += 1
-            obj = fetch_function( obj_id )
+            if inherited:
+                obj = fetch_function(self, obj_id)
+            else:
+                obj = fetch_function(obj_id)
             f = open(cache_file, "w")
             f.write(obj)
             f.close()
@@ -91,21 +101,35 @@ class Cache(object):
 class IterableCache(Cache):
     """ caches arbitrary iterable content identified by an identifier """
 
+    cache_file      = ""
+    _cls            = None
+    _fetch_function = None
+    _cached         = False
+    _seq            = 0
+ 
     def __iter__(self): return self
 
-    def fetch(self, obj_id, fetch_function):
+    def fetch(self, obj_id, fetch_function, cls):
         """ checks whether the object with the given id exists """
         self.cache_file = self._get_fname( self.getObjectId(obj_id) )
-        self.seq = 0
+        self._seq = 0
 
         if exists(self.cache_file):
-            self.next = self._read_next_element()
+            self._cached = True
         else:
-            self._fetch_function = fetch_function(obj_id)
-            self.next = self._cache_next_element()
+            self._cls = cls
+            self._fetch_function = fetch_function(self, obj_id)
+            self._cached = False
             open( self.cache_file, "w").close()
 
         return self
+
+
+    def next(self):
+        if self._cached:
+            return self._read_next_element()
+        else:
+            return self._cache_next_element()
 
 
     def _cache_next_element(self):
@@ -114,10 +138,10 @@ class IterableCache(Cache):
             c) passes the data through to the calling element
         """
         self._cache_miss += 1
-        obj = self._fetch_function.next()
-        f = open( "%s.%d" % (self.cache_file, self.seq), "w")
-        f.write(obj)
-        f.close()
+        obj = attrgetter('next')(self._cls)(self)
+        f = open( "%s.%d" % (self.cache_file, self._seq), "w")
+        cPickle.dump(obj, f)
+        self._seq +=1
         return obj
 
 
@@ -125,8 +149,8 @@ class IterableCache(Cache):
         """ returns the next element from the cache """
         self._cache_hit +=1
         try:
-            obj = open( "%s.%d" % (self.cache_file, self.seq) ).read()
-            self.seq += 1
+            obj = cPickle.load(open( "%s.%d" % (self.cache_file, self._seq) ) )
+            self._seq += 1
             return obj
         except IOError:
             raise StopIteration
