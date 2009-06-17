63cdd5e7e4b44d2db7c3c69746c1f5b7878d26f7
heinz <heinz@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-06-17 10:19:25 +0000

    added Google Trends

---
+++ src/ws/googletrends/__init__.py
@@ -0,0 +1,37 @@
+from pyGTrends import pyGTrends
+from config import GOOGLE_USER, GOOGLE_PASS
+
+## Documentation for the Class Google Trends
+#
+# fetches the Google trends for a set of keywords
+class GoogleTrends(object):
+
+    ## Constructor establishes the connection
+    # @param user
+    # @param password
+    def __init__(self, user=GOOGLE_USER, password=GOOGLE_PASS):
+        """ Constructor establishes the connection """
+        self.connector = pyGTrends(user, password)
+
+    ## getTrends fetches the trends
+    # @param keywords
+    # @param date to filter 
+    # @param section
+    # @return 
+    def getTrends(self, keywords, date=None, section=None):
+        if date == None:
+            self.connector.download_report(keywords)
+        else:
+            self.connector.download_report(keywords, date)
+
+        if section == None:
+            trends = self.connector.csv()
+        else:
+            trends = self.connector.csv()
+
+        return trends
+
+if __name__ == "__main__":
+    a = GoogleTrends()
+    print a.printTrends(('Terminator', 'Rambo'))
+    print a.getTrends(('Apple', 'Microsoft'))
