6111c2477b4be87ebd58d922a564ca2cbf693bb0
heinz <heinz@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-06-29 07:36:27 +0000

    added support for twitter trends

---
+++ src/ws/twitter-trends/__init__.py
@@ -0,0 +1,13 @@
+import tango
+
+## @package TwitterTrends
+class TwitterTrends(object):
+
+    # init connects to Twitter
+    def __init__(self):
+        self.twitter = tango.setup();
+
+    # getDailyTrends fetches the daily trends of twitter
+    # @return trends
+    def getDailyTrends(self):
+        return self.twitter.getDailyTrends() 
