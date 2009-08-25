ebc607fe3e3ba91f6a192428af22c0ecbd57d5d7
heinz <heinz@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-08-25 09:22:50 +0000

    implemented tagInfo

--- src/ws/delicious/__init__.py
+++ src/ws/delicious/__init__.py
@@ -22,17 +22,18 @@ __version__ = "$Header$"
 import sys
 import re
 from eWRT.access.http import Retrieve
-from eWRT.ws import TagInfoService
+from eWRT.ws.TagInfoService import TagInfoService
 from urlparse import urlsplit
 from md5 import md5
 from eWRT.config import DELICIOUS_USER, DELICIOUS_PASS
-
 
 class Delicious(TagInfoService):
     """ retrieves data using the del.icio.us API """
     
     DELICIOUS_SERVICE_URL = "http://del.icio.us/url/%s"
+    DELICIOUS_TAG_URL = "http://delicious.com/tag/%s"
     RE_COUNT = re.compile("this url has been saved by (\d+) people")
+    RE_TAG_COUNT = re.compile("<p>(\d+) Bookmarks</p></div>")
 
     @staticmethod
     def getUrlInfo( url ):
@@ -45,16 +46,27 @@ class Delicious(TagInfoService):
         """ @param   tags   A list of tags to retrieve information for
             @returns        the number of bookmarks using the given tags
         """
-        raise NotImplementedError
+
+        url = Delicious.DELICIOUS_TAG_URL % "+".join(tags)        
+        content = Delicious.get_content(url)
+        return Delicious._parse_tag_counts(content)
 
     # 
     # helper functions
     #
-
     @staticmethod
     def _parse_counts( content ):
         """ parses del.icio.us's html content and returns the number of counts """
         m=Delicious.RE_COUNT.search( content )
+        if m:
+            return m.group(1)
+        else:
+            return 0
+
+    @staticmethod
+    def _parse_tag_counts( content ):
+        """ parses del.icio.us's html content and returns the number of counts for the tags """
+        m=Delicious.RE_TAG_COUNT.search( content )
         if m:
             return m.group(1)
         else:
@@ -67,9 +79,9 @@ class Delicious(TagInfoService):
             url += "/"
         return url
 
+    # heinz: todo remove or adopt???
     @staticmethod
     def delicious_info_retrieve( url ):
-        """ returns the data from delicious """
         assert( url.startswith("http") )
 
         md5_url = md5( Delicious._normalize_url(url)).hexdigest()
@@ -80,9 +92,20 @@ class Delicious(TagInfoService):
         f.close()
 
         return Delicious._parse_counts(content)
+    
+    @staticmethod
+    def get_content( url ):
+        """ returns the content from delicious """
+        assert( url.startswith("http") )
 
+        f = Retrieve(Delicious.__name__).open(url)
+        content = f.read()
+        f.close()
+        return content
 
 if __name__ == '__main__':
     url = sys.argv[1].strip()
+    print Delicious.delicious_get_content( url )
     print Delicious.getUrlInfo( url ), "counts"
+    print Delicious.getTagInfo( ("debian", "linux") ), "counts"
 
