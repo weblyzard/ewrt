a28df5f391d1ed546e4b2dd2b975ba9c89670d67
albert <albert@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-08-27 09:35:39 +0000

    fixed unicode handling error

--- src/ws/opencalais/__init__.py
+++ src/ws/opencalais/__init__.py
@@ -134,6 +134,7 @@ class Calais:
 
         things = []
 
+        f= open("tmp","w"); f.write(xml_data.encode("utf8")); f.close()
         dom = minidom.parseString( xml_data.encode("utf8" ))
         for document in dom.getElementsByTagName("CalaisSimpleOutputFormat"):
             for annotations in document.childNodes:
