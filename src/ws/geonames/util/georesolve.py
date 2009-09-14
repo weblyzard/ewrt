c2a9eda2d1aa58142dee479ac10a92028992a26d
albert <albert@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-09-14 09:57:15 +0000

    improved name lookup routine

--- src/ws/geonames/util/georesolve.py
+++ src/ws/geonames/util/georesolve.py
@@ -28,12 +28,16 @@ from eWRT.util.cache import MemoryCached
 from eWRT.config import DATABASE_CONNECTION
 # from warnings import warn
 
+MIN_POPULATION = 5000
+
 class GazetteerEntryNotFound(Exception):
     """ @class GazetteerEntryNotFound
         Base class for gazetteer lookup errors 
     """
-    def __init__(self, id):
+    def __init__(self, id, query):
         self.id = id
+        self.query = query
+        print id, query
 
     def __str__(self):
         return "Gazetteer lookup for entity-id '%s' failed." % (self.id)
@@ -115,8 +119,10 @@ class Gazetteer(object):
             @param string
             @return dictionary of locations
         """
+        Gazetteer.DEBUG = True
         res = set()
-        query = '''SELECT entity_id FROM gazetteerentry JOIN hasname ON (gazetteerentry.id = hasname.entry_id) WHERE name = '%s' '''
+        query = '''SELECT entity_id FROM gazetteerentry JOIN hasname ON (gazetteerentry.id = hasname.entry_id) 
+                  JOIN gazetteerentity ON (gazetteerentity.id=hasname.entity_id) WHERE name = '%%s' AND population > %d''' % ( MIN_POPULATION)
         for result in self.db.query(query % name.replace("'", "''")):
             try:
                 tmp = Gazetteer.getGeoNameFromGazetteerID(self, result['entity_id'])
@@ -159,7 +165,7 @@ class Gazetteer(object):
             @returns the geo entity's name
         """ 
 
-        query = '''SELECT name FROM vw_gazetteer_c5000_tng WHERE id=%d 
+        query = '''SELECT name FROM vw_gazetteer_tng WHERE id=%d 
                       ORDER BY 
                             (lang='en' and lang is not null) DESC,
                             (short=TRUE and short IS NOT NULL) DESC,
@@ -167,7 +173,7 @@ class Gazetteer(object):
                       LIMIT 1''' % (id)
         result = self.db.query(query)
         if not result:
-            raise GazetteerEntryNotFound(id)
+            raise GazetteerEntryNotFound(id, query)
 
         return Gazetteer.DEBUG and result[0]['name']+"(%d)" % (id) or result[0]['name']
 
@@ -200,9 +206,7 @@ class TestGazeteer(object):
     
     def testUniqueStringResolver(self):
         getName = Gazetteer.getGeoNameFromString
-        assert getName( self.gazetteer, "Lainach" ) == []
-
-        for name, entries in ( ('Vienna', 3), ('Madrid', 2), ('Graz', 1), ('Poland', 2), ('Geneva', 4) ):
+        for name, entries in ( ('Lainach', 0), ('Vienna', 4), ('Madrid', 5), ('Graz', 1), ('Poland', 2), ('Geneva', 5) ):
             assert len( getName( self.gazetteer, name )) == entries 
 
     def testContentIdResolver(self):
@@ -219,5 +223,6 @@ if __name__ == "__main__":
         print Gazetteer.getGeoNameFromString(a, "Lainach" )
         print Gazetteer.getGeoNameFromString(a, "Spittal an der Drau" )
         print Gazetteer.getGeoNameFromString(a, "Salzburg" )
+        print Gazetteer.getGeoNameFromString(a, "London" )
 
 
