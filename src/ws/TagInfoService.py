9dfe34ae741b9aa934cba5de0a6e1f5d73606178
heinz <heinz@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-09-21 08:58:03 +0000

    extended abstract class with the function getRelatedTags

--- src/ws/TagInfoService.py
+++ src/ws/TagInfoService.py
@@ -1,8 +1,13 @@
-## Class for fetching assigned tags
 class TagInfoService(object):
+    """ Class for fetching assigned tags """
     
-    ## returns the count for the given tags
-    # @param list/tuple of tags
-    # @return number of counts
     def getTagInfo( tags ):
+        """ returns the count for the given tags
+            @param list/tuple of tags
+            @returns number of counts """ 
         return NotImplemented        
+
+    def getRelatedTags( tags ):
+        """ returns a the count of related tags 
+            @param list/tuple of tags 
+            @returns list of related tags with a count of their occurence """
