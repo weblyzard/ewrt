25ac22afad2b4c7897feb98cd59c47706a7464e1
heinz <heinz@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-09-21 14:27:21 +0000

    renamed TwitterTrends to Twitter started implementing getRelatedTags

--- src/ws/twittertrends/__init__.py
+++ src/ws/twittertrends/__init__.py
@@ -1,8 +1,14 @@
 from eWRTlibs.tango.tango import tango
 import oauth
 
+from eWRT.access.http import Retrieve
+from eWRT.ws.TagInfoService import TagInfoService
+import json
+
 ## @package TwitterTrends
-class TwitterTrends(object):
+class Twitter(TagInfoService):
+
+    TWITTER_SEARCH_URL = 'http://search.twitter.com/search.json?q=&tag=%s&lang=all'
 
     # init connects to Twitter
     def __init__(self):
@@ -12,3 +18,17 @@ class TwitterTrends(object):
     # @return trends
     def getDailyTrends(self):
         return self.twitter.getDailyTrends() 
+
+    @staticmethod
+    def getRelatedTags( tags ):
+        url = Twitter.TWITTER_SEARCH_URL % "+".join(tags)
+        f = Retrieve(Twitter.__name__).open(url)
+        search_result = f.read()
+        result = json.loads(search_result) 
+
+        # todo: continue here: extract tags
+
+        print result
+
+if __name__ == '__main__':
+    Twitter.getRelatedTags(('debian', 'linux'))
