2cf9a1fd551f6c1958ad23d4914bc604192e1915
duong <duong@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-10-21 08:05:40 +0000

    final version by Johannes Duong

--- src/ws/geonames/util/relate.py
+++ src/ws/geonames/util/relate.py
@@ -43,22 +43,293 @@ class MoreSpecific(Related):
     """ Bsp: (eu/at, eu/at/Carinthia/Klagenfurt) 
         => more specific 
     """
+    
+    def __init__(self):
+    	self.setRelationWeight(( 1.0, 1.0, 0.9, 0.9, 0.85 ))
+
+    def isRelated( self, geoRef1, geoRef2):
+    	entries_of_geoRef1 = ()
+	entries_of_geoRef2 = ()
+	for id, value in geoRef1:
+		entries_of_geoRef1 = value
+		
+	for id, value in geoRef2:
+		entries_of_geoRef2 = value
+		
+	relation_strength = 0
+	print len(entries_of_geoRef1)
+	print len(entries_of_geoRef2)
+	weight = self.weights
+	
+	if entries_of_geoRef1[0] == entries_of_geoRef2[0]:
+		if len(entries_of_geoRef2) > len(entries_of_geoRef1):
+			diff = len(entries_of_geoRef2) - len(entries_of_geoRef1)
+			if len(entries_of_geoRef2) == 5:     
+				if diff == 4:
+					relation_strength = weight[diff] * weight[diff-1] * weight[diff-2] * weight[diff-3]
+					print relation_strength
+					return relation_strength
+				elif diff == 3:
+					relation_strength = weight[diff] * weight[diff-1] * weight[diff+1]
+					print relation_strength
+					return relation_strength
+				elif diff == 2:
+					relation_strength = weight[diff+1] * weight[diff+2]
+					print relation_strength
+					return relation_strength
+				elif diff == 1:
+					relation_strength = weight[diff+3]
+					print relation_strength
+					return relation_strength
+           		elif len(entries_of_geoRef2) == 4:
+				if diff == 3:
+					relation_strength = weight[diff] * weight[diff-1] * weight[diff-2]
+					print relation_strength
+					return relation_strength
+				elif diff == 2:
+						relation_strength = weight[diff] * weight[diff+1]
+						print relation_strength
+						return relation_strength
+				elif diff == 1:
+						relation_strength = weight[diff+2]
+						print relation_strength
+						return relation_strength
+            		elif len(entries_of_geoRef2) == 3:
+				if diff == 2:
+					relation_strength = weight[diff] * weight[diff-1]
+					print relation_strength
+					return relation_strength
+				elif diff == 1:
+					relation_strength = weight[diff+1]
+					print relation_strength
+					return relation_strength
+			elif len(entries_of_geoRef2) == 2:
+				if diff == 1:
+					relation_strength = weight[diff]
+					print relation_strength
+					return relation_strength
+                  
+        else:
+                print relation_strength
+                return relation_strength
 
     def setRelationWeight( self, weights ):
-        self.weights = ( 1.0, 1.0, 0.9, 0.9, 0.85 )
+        self.weights = weights
 
+	
 
 
 class LessSpecific(Related):
-    """ Bsp: (eu/at/Styria/Graz, eu/at/Styria)
+	""" Bsp: (eu/at/Styria/Graz, eu/at/Styria)
         => less specific """
+	"""
+f.weights = ( 0.1, 0.3, 0.8, 0.95 )
+	"""
+	
+	def __init__(self):
+        	self.setRelationWeight(( 0.1, 0.3, 0.8, 0.95 ))
 
-    def setRelationWeight( weights ):
-        self.weights = ( 0.1, 0.3, 0.8, 0.95 )
+    	def isRelated( self, geoRef1, geoRef2):
+		entries_of_geoRef1 = ()
+		entries_of_geoRef2 = ()
+		for id, value in geoRef1:
+			entries_of_geoRef1 = value
+	
+		for id, value in geoRef2:
+			entries_of_geoRef2 = value
+	
+		relation_strength = 0
+		print len(entries_of_geoRef1)
+		print len(entries_of_geoRef2)
+		weight = self.weights
+	
+		if entries_of_geoRef1[0] == entries_of_geoRef2[0]:
+			if len(entries_of_geoRef2) < len(entries_of_geoRef1):
+				diff = len(entries_of_geoRef1) - len(entries_of_geoRef2)
+				if len(entries_of_geoRef1) == 5:
+					if diff == 4:
+						relation_strength = weight[diff] * weight[diff-1] * weight[diff-2] * weight[diff-3]
+						print relation_strength
+						return relation_strength
+					elif diff == 3:
+						relation_strength = weight[diff] * weight[diff-1] * weight[diff+1]
+						print relation_strength
+						return relation_strength
+					elif diff == 2:
+						relation_strength = weight[diff+1] * weight[diff+2]
+						print relation_strength
+						return relation_strength
+					elif diff == 1:
+						relation_strength = weight[diff+3]
+						print relation_strength
+						return relation_strength
+				elif len(entries_of_geoRef1) == 4:
+					if diff == 3:
+						relation_strength = weight[diff] * weight[diff-2] * weight[diff+1]
+						print relation_strength
+						return relation_strength
+					elif diff == 2:
+						relation_strength = weight[diff] * weight[diff+1]
+						print relation_strength
+						return relation_strength
+					elif diff == 1:
+						relation_strength = weight[diff+2]
+						print relation_strength
+						return relation_strength
+				elif len(entries_of_geoRef1) == 3:
+					if diff == 2:
+						relation_strength = weight[diff] * weight[diff-1]
+						print relation_strength
+						return relation_strength
+					elif diff == 1:
+						relation_strength = weight[diff+1]
+						print relation_strength
+						return relation_strength
+				elif len(entries_of_geoRef1) == 2:
+					if diff == 1:
+						relation_strength = weight[diff]
+						print relation_strength
+						return relation_strength
+	
+		else:
+			print relation_strength
+			return relation_strength
+
+    	def setRelationWeight( self, weights ):
+        	self.weights = weights
+
 
 
 class MoreOrLessSpecific(Related):
     """ combines less and more specific """
+
+    def __init__(self):
+    	self.setRelationWeight(( 1.0, 1.0, 0.9, 0.9, 0.85 ))
+    	self.setRelationWeightl(( 0.1, 0.3, 0.8, 0.95, 1.5 ))
+
+
+    def isRelated( self, geoRef1, geoRef2):
+    	entries_of_geoRef1 = ()
+	entries_of_geoRef2 = ()
+	for id, value in geoRef1:
+		entries_of_geoRef1 = value
+		
+	for id, value in geoRef2:
+		entries_of_geoRef2 = value
+		
+	relation_strength = 0
+	print len(entries_of_geoRef1)
+	print len(entries_of_geoRef2)
+	weight = self.weights
+	weightl = self.weightsl
+	
+	if entries_of_geoRef1[0] == entries_of_geoRef2[0]:
+		if len(entries_of_geoRef2) > len(entries_of_geoRef1):
+			diff = len(entries_of_geoRef2) - len(entries_of_geoRef1)
+			if len(entries_of_geoRef2) == 5:     
+				if diff == 4:
+					relation_strength = weight[diff] * weight[diff-1] * weight[diff-2] * weight[diff-3]
+					print relation_strength
+					return relation_strength
+				elif diff == 3:
+					relation_strength = weight[diff] * weight[diff-1] * weight[diff+1]
+					print relation_strength
+					return relation_strength
+				elif diff == 2:
+					relation_strength = weight[diff+1] * weight[diff+2]
+					print relation_strength
+					return relation_strength
+				elif diff == 1:
+					relation_strength = weight[diff+3]
+					print relation_strength
+					return relation_strength
+           		elif len(entries_of_geoRef2) == 4:
+				if diff == 3:
+					relation_strength = weight[diff] * weight[diff-1] * weight[diff-2]
+					print relation_strength
+					return relation_strength
+				elif diff == 2:
+						relation_strength = weight[diff] * weight[diff+1]
+						print relation_strength
+						return relation_strength
+				elif diff == 1:
+						relation_strength = weight[diff+2]
+						print relation_strength
+						return relation_strength
+            		elif len(entries_of_geoRef2) == 3:
+				if diff == 2:
+					relation_strength = weight[diff] * weight[diff-1]
+					print relation_strength
+					return relation_strength
+				elif diff == 1:
+					relation_strength = weight[diff+1]
+					print relation_strength
+					return relation_strength
+			elif len(entries_of_geoRef2) == 2:
+				if diff == 1:
+					relation_strength = weight[diff]
+					print relation_strength
+					return relation_strength
+                elif len(entries_of_geoRef2) < len(entries_of_geoRef1):
+			diff = len(entries_of_geoRef1) - len(entries_of_geoRef2)
+			if len(entries_of_geoRef1) == 5:
+				if diff == 4:
+					relation_strength = weightl[diff] * weightl[diff-1] * weightl[diff-2] * weightl[diff-3]
+					print relation_strength
+					return relation_strength
+				elif diff == 3:
+					relation_strength = weightl[diff] * weightl[diff-1] * weightl[diff+1]
+					print relation_strength
+					return relation_strength
+				elif diff == 2:
+					relation_strength = weightl[diff+1] * weightl[diff+2]
+					print relation_strength
+					return relation_strength
+				elif diff == 1:
+					relation_strength = weightl[diff+3]
+					print relation_strength
+					return relation_strength
+			elif len(entries_of_geoRef1) == 4:
+				if diff == 3:
+					relation_strength = weightl[diff] * weightl[diff-2] * weightl[diff+1]
+					print relation_strength
+					return relation_strength
+				elif diff == 2:
+					relation_strength = weightl[diff] * weightl[diff+1]
+					print relation_strength
+					return relation_strength
+				elif diff == 1:
+					relation_strength = weightl[diff+2]
+					print relation_strength
+					return relation_strength
+			elif len(entries_of_geoRef1) == 3:
+				if diff == 2:
+					relation_strength = weightl[diff] * weightl[diff-1]
+					print relation_strength
+					return relation_strength
+				elif diff == 1:
+					relation_strength = weightl[diff+1]
+					print relation_strength
+					return relation_strength
+			elif len(entries_of_geoRef1) == 2:
+				if diff == 1:
+					relation_strength = weightl[diff]
+					print relation_strength
+					return relation_strength
+	
+		else:
+			print relation_strength
+			return relation_strength 
+        else:
+                print relation_strength
+                return relation_strength
+
+    def setRelationWeight( self, weights ):
+        self.weights = weights
+
+    def setRelationWeightl( self, weightsl ):
+        self.weightsl = weightsl
+
 
 
 
@@ -69,3 +340,20 @@ class TestRelated(object):
         r = Related()
         assert r.isRelated( "eu/at/Vienna/Vienna", "eu/at/Styria" ) == 0.7 
 
+if __name__ == "__main__":
+	
+	#ms = MoreSpecific()
+	#a = [(90610L, ('Europe', 'Austria', 'Carinthia', 'Klagenfurt'))]
+	#b = [(8184691L, ('Europe', 'Austria'))]
+	#ms.isRelated(b,a)
+	
+	#ms = LessSpecific()
+	#a = [(90610L, ('Europe', 'Austria', 'Carinthia', 'Klagenfurt'))]
+	#b = [(8184691L, ('Europe', 'Austria'))]
+	#ms.isRelated(a,b)
+	
+	ms = MoreOrLessSpecific()
+	a = [(90610L, ('Europe', 'Austria', 'Carinthia', 'Klagenfurt'))]
+	b = [(8184691L, ('Europe', 'Austria'))]
+	ms.isRelated(a,b)
+	
