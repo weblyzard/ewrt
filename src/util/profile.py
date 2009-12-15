2872dfab1eb5edf82a38c8f41b4162096ed02cc3
albert <albert@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-12-15 08:33:09 +0000

    changed to name

--- src/util/profile.py
+++ src/util/profile.py
@@ -29,10 +29,10 @@ __copyright__ = "GPL"
 from StringIO import StringIO
 import cProfile, pstats
 
-def profile(fnName, logfile="profile.awi"):
+def profile(fn, logfile="profile.awi"):
     """ profile function """
     prof = cProfile.Profile()
-    prof = prof.runctx(fnName, globals(), locals())
+    prof = prof.runctx("%s()" % fn.__name__, globals(), locals())
     stream = StringIO()
     stats = pstats.Stats(prof, stream=stream)
     stats.sort_stats("time")  # Or cumulative
