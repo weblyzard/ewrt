026dbe228e0c47c0144a0610921793f525f8ee40
heinz <heinz@52f8a927-27e1-4e1c-b33e-807c988f9568>
2010-11-02 11:57:55 +0000

    - removed import of facebook library -> instead using eWRT.access.http.Retrieve to fetch the data - all required features should work now :)

--- src/eWRT/ws/facebook/__init__.py
+++ src/eWRT/ws/facebook/__init__.py
@@ -19,102 +19,130 @@ class FacebookWS(Webservice.Webservice):
     """ class for fetching and storing the data of a user
     requires that the facebook API key and the facebook secret key are
     set in the configuration file. These can be retrieved from facebook
-    todo: add url
-    issues: the loginprocess opens a browser window every time the pragram is started
-    todo: describe usage of Result and ResultSet
     """
-
-    albums = None
-    friends = None
-    groups = None
+    FB_OBJECT_TYPES = ['post', 'user', 'page', 'event', 'group']
 
     def __init__(self):
         """ init """       
-        self.login()
+        self.retrieve = Retrieve('facebookWS')
 
 
-    def login(self):
-        """ opens a browserwindow to login at facebook """
-        if FACEBOOK_SESSION_KEY == '':
-            self.facebook = Facebook(FACEBOOK_API_KEY, FACEBOOK_SECRET_KEY)
-            self.facebook.auth.createToken()
-            self.facebook.login()
+    def search(self, term, objectType='all'):
+        '''
+        searches for the given term 
+        @param term: term to search
+        @param objectType: objectType to search in (post, user, page, event, group)
+        @return: search result
+        '''
+        args = {}
+        result= []
+        
+        args['q'] = term
+        
+        if objectType in self.FB_OBJECT_TYPES:
+            args['type'] = objectType
+            result = self.makeRequest('search', args)
         else:
-            print 'Infinite session key found'
-            self.facebook = Facebook(FACEBOOK_API_KEY, FACEBOOK_SECRET_KEY)
-            self.facebook.session_key = FACEBOOK_SESSION_KEY
-
- 
-    def loadAlbums(self, userID=None, loadPhotos=0):    
-        """ fetches all albums for a user """
-
-        if userID is None:
-            userID = self.facebook.uid
-
-        self.albums = ResultSet(None, 'Albums')
-
-        loadedAlbums = self.facebook.photos.getAlbums(userID)
-
-        for album in loadedAlbums:
-            id = album['aid']
-            name = album['name']
-            newAlbum = ResultSet(id, name)
-            self.albums.addContent(newAlbum)
-
-            if loadPhotos is 1:
-                self.loadPhotos(newAlbum)
-
-
-    #def loadPhotos(self, albumID, userID=None):
-    def loadPhotos(self, album):
-        """ fetches all photos in an album """
-        
-        photos = self.facebook.photos.get(album.getId())
-
-        for photo in photos:
             
-            newPhoto = ResultSet(photo['pid'], photo['name'], photo['tags'])
+            # search all object types
+            for type in self.FB_OBJECT_TYPES:
+                args['type'] = type
+                result.extend(self.makeRequest('search', args))
 
-            album.addContent(newPhoto)       
+        return result
 
 
-    def getFriends(self):
-        """ get friends """
-        self.friends = ResultSet(None, 'Friends')
+    def makeRequest(self, path, args={}):
+        '''
+        makes a request to the graph API
+        @param path: path to query
+            e.g. userprofile of current user: me
+                 feed of user/group/page 122222: 122222/feed
+        @return: fetched data
+        '''
+        
+        if not args.has_key('access_token'):
+            args['access_token'] = FACEBOOK_ACCESS_KEY
+        
+        url = "https://graph.facebook.com/%s?%s" % (path, urllib.urlencode(args)) 
+        
+        return self.requestURL(url)
 
-        friendList = self.facebook.friends.get()
 
-        friendList = self.facebook.users.getInfo(friendList)
+    def requestURL(self, url):
+        '''
+        fetches the data for the give URL from the graph API
+        @param url: valid graph-api-url
+        @return: fetched data 
+        '''
+        
+        result = []
+        
+        try:
+        
+            file = self.retrieve.open(url)
+            fetched = json.loads(file.read())
+            
+            
+            logging.debug('processing url %s' % url)
+            
+            if fetched.has_key('data'):
+                
+                # process paging
+                if fetched.has_key('paging'):
+                    if fetched['paging'].has_key('previous'):
+                        result.extend(self.requestURL(fetched['paging']['next']))
+                
+                result.extend(fetched['data'])
+            else:
+                # profiles for example don't contain a data dictionary
+                result.append(fetched)
+        
+        except HTTPError:
+            print 'Error: Bad Request for url', url
+        
+        return result
 
-        for friend in friendList:
+class TestFacebookWS(unittest.TestCase):
 
-            friendItem = Result(friend['uid'], friend['name'])
-            self.friends.addContent(friendItem)
+    def setUp(self):
+        ''' setup the webservice '''
+        self.fb = FacebookWS()
+        
+    def testSearch(self):
+        ''' tests the search '''
+        resultPage = self.fb.search('Linus Torvalds', 'page')
+        assert len(resultPage) > 5
+        assert resultPage[0]['name'] == 'Linus Torvalds'
+
+        resultAll = self.fb.search('Linus Torvalds')
+        assert len(resultAll) > len(resultPage)
+
+        result = self.fb.search('Heinz Lang', 'user')
+        assert len(result) > 5
+
+    def testFetchingProfile(self):
+        ''' tests fetching the profile '''
+        result = self.fb.makeRequest('me')
+        assert len(result) == 1
+        assert result[0].has_key('first_name')
+
+    def testRequestUrl(self):
+        ''' tests request url '''
+        url = 'https://graph.facebook.com/me?access_token=%s' % FACEBOOK_ACCESS_KEY
+        result = self.fb.requestURL(url)
+        assert len(result) == 1
+        assert result[0].has_key('first_name')
         
 
-    def getGroups(self):
-        """ fetches the groups of the user """
-        self.groups = ResultSet(None, 'Groups')
+    def testBadRequest(self):
+        ''' tests that a bad request won't disturb the program'''
 
-        groupList = self.facebook.groups.get()
-
-        for group in groupList:
-
-            newGroup = Result(group['gid'], group['name'])
-            self.groups.addContent(newGroup)
-
-    
-    def loadAllData(self):
-        """ runs all functions to fetch all data """
-        self.getFriends()
-        self.loadAlbums(1)
-        self.getGroups()
-
-    def printAllData(self):
-        self.loadAllData()
-        ResultSet.printRS(self.friends)
-        ResultSet.printRS(self.albums)
-        ResultSet.printRS(self.groups)
+        # this URL contains an invalid access token
+        url = 'https://graph.facebook.com/me?access_token=2227470867%7C2._jNijjNpTLF_OrmDqYTEA__.3600.1288695600-1145817399%7CECkapj6t0eZK8DjnNfSVRANS8lI'
+        result = self.fb.requestURL(url)
+        assert [] == result
+        
 
 
     def getObjectWall(self, objId, object=None):
