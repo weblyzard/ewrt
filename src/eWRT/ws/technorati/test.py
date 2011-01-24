32709f17fb8623f76885fd3ebf7cfd4c38aee80e
heinz <heinz@52f8a927-27e1-4e1c-b33e-807c988f9568>
2011-01-24 15:49:18 +0000

    added all tests for technorati

--- src/eWRT/ws/technorati/test.py
+++ src/eWRT/ws/technorati/test.py
@@ -7,36 +7,36 @@ TECHNORATI_TEST_TAGS = ['linux', ('debian', 'linux') ]
 
 class TestTechnorati( unittest.TestCase ):
 
-#    def test_tag_info(self):
-#        print '### Testing tag_info ###'
-#        
+    def test_tag_info(self):
+        print '### Testing tag_info ###'
+        
+        countTags = 0
+        
+        for tags in TECHNORATI_TEST_TAGS:
+            countTags = Technorati.getTagInfo( tags )
+            print '%s has %s counts ' % (tags, countTags)
+
+            assert countTags > 0
+
+
+    
+    def test_related_tags(self):
+        print '### Testing related_tags ###'
+        
+        print 'not supported by Technorati at the moment'
+        
 #        countTags = 0
 #        
 #        for tags in TECHNORATI_TEST_TAGS:
-#            countTags = Technorati.getTagInfo( tags )
-#            print '%s has %s counts ' % (tags, countTags)
-#
-#            assert countTags > 0
-#
-#
-#    
-#    def test_related_tags(self):
-#        print '### Testing related_tags ###'
-#        
-#        print 'not supported by Technorati at the moment'
-#        
-##        countTags = 0
-##        
-##        for tags in TECHNORATI_TEST_TAGS:
-##            countTags = Technorati.getRelatedTags( tags )
-##            print '%s has related tags: %s' % (tags, countTags)
-##        assert countTags > 0
-#
-#    def test_parse_url(self):
-#        
-#        print '### Test parsing URL ###'
-#        assert Technorati._parseURL('linux') == 'http://technorati.com/search?usingAdvanced=1&q=linux&return=posts&source=advanced-source-all&topic=overall&authority=high'
-#        assert Technorati._parseURL(('debian', 'linux')) == 'http://technorati.com/search?usingAdvanced=1&q=debian+linux&return=posts&source=advanced-source-all&topic=overall&authority=high'
+#            countTags = Technorati.getRelatedTags( tags )
+#            print '%s has related tags: %s' % (tags, countTags)
+#        assert countTags > 0
+
+    def test_parse_url(self):
+        
+        print '### Test parsing URL ###'
+        assert Technorati._parseURL('linux') == 'http://technorati.com/search?usingAdvanced=1&q=linux&return=posts&source=advanced-source-all&topic=overall&authority=high'
+        assert Technorati._parseURL(('debian', 'linux')) == 'http://technorati.com/search?usingAdvanced=1&q=debian+linux&return=posts&source=advanced-source-all&topic=overall&authority=high'
 
     def test_get_blog_links(self):
         ''' tests retrieving the blog links '''
