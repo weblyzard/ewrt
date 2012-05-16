d9f266e8f0f8d5aff85a7dd80e59f1e2056fb5ff
Albert Weichselbraun <albert@weichselbraun.net>
2012-05-16 13:05:03 +0200

    added stock data source

---
+++ src/eWRT/input/stock/__init__.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""
+@package: eWRT.input.stock
+Provides access to historical stock market data
+
+Interesting indices to support in the future:
+* BALTIC DRY INDEX (BDIY:IND)
+* Howe Robinson Container Index
+"""
+
+from bz2 import BZ2File
+from csv import reader
+from glob import glob
+from os.path import dirname, basename, join as os_join
+from collections import namedtuple
+from datetime import datetime
+from twisted.python.text import strFile
+
+extract_index_name = lambda fname: basename(fname).split(".")[0]
+DATA_DIR = os_join( dirname(__file__), 'data' )
+
+Quote = namedtuple('quote', 'date last open high low change_percentage')
+
+
+class StockIndex(object):
+    SUPPORTED_INDICES = { extract_index_name(fname):fname 
+                         for fname in glob(DATA_DIR+"/*.idx.bz2")  }
+    SUPPORTED_FUTURES = { extract_index_name(fname):fname 
+                         for fname in glob(DATA_DIR+"/*.ftidx.bz2")  }
+    DATE_FORMAT = "%b %d, %Y"
+
+    @classmethod
+    def get_index(cls, index_name):
+        """retrieves data from the given index in csv format
+           @param index_name: the name of the index
+        """
+        return cls._read_index_filen(cls.SUPPORTED_INDICES[index_name])
+    
+    
+    @classmethod
+    def get_future(cls, future_name):
+        """retrieves data from the given future in csv format
+           @param future_name: the name of the future
+        """
+        return cls._read_index_filen(cls.SUPPORTED_FUTURES[future_name])
+        
+        
+    @classmethod
+    def _read_index_filen(cls, fname):
+        """ @return: the contents of the given index file.
+        """
+        with BZ2File(fname) as f:
+            result =[Quote._make(cls._convert_tuple(row)) 
+                     for row in reader(f, delimiter="\t")] 
+            
+        return result
+    
+    
+    @classmethod
+    def _convert_tuple(cls, t):
+        """ converts the string in the given tuple to the respective 
+            float/_date datatypes.
+            @param t: the input tuple
+            @return: output tuple with the correct datatypes 
+        """
+        #_date _last _open _high _low %_change'
+        _date, _last, _open, _high, _low, _change = t
+        return (datetime.strptime(_date, cls.DATE_FORMAT), float(_last),
+                float(_open), float(_high), float(_low), float(_change[:-1]))
+        
+    
+if __name__ == '__main__':
+    s = StockIndex.get_index('dax')
+    print len(s), s
