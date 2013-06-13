a8c8bdc3a0a2b918e894476ff47b0a421bc5824a
Daniel Streiff <daniel.streiff@htwchur.ch>
2013-06-13 11:05:30 +0200

    adds the WeblyzardService

---
+++ src/eWRT/ws/rest/server/example.py
@@ -0,0 +1,43 @@
+﻿'''
+Created on 12.06.2013
+
+@author: streifdaniel
+'''
+
+#@see http://www.stereoplex.com/blog/understanding-imports-and-pythonpath
+import sys
+sys.path[0] = '/home/stredani/EclipseWorkspace/eWRT/src'
+del sys.path[3]
+#print '\n'.join(sys.path)
+
+from eWRT.ws.rest.server.service import WeblyzardService
+
+class HelloWorld(WeblyzardService):
+    
+    DEFAULT_PATH = "helloworld";
+    
+    isLeaf = True
+    
+    def __init__(self, cfg=None):
+        WeblyzardService.__init__(self, self.hello_world)
+
+    def hello(self):
+        return "Hello"
+    
+    def hello_world(self, name = "World"):
+        ''' Returns "Hello [name]"
+            
+            @param name: the name to say hi to
+            @return: greetings
+        '''
+        return "Hello " + name
+
+if __name__ == '__main__':
+    from twisted.web import server
+    from twisted.internet import reactor
+    
+    root = HelloWorld()
+    root.putChild(HelloWorld.DEFAULT_PATH, HelloWorld())
+    server = server.Site(root)
+    reactor.listenTCP(8123, server)
+    reactor.run()
---
+++ src/eWRT/ws/rest/server/exception.py
@@ -0,0 +1,26 @@
+﻿'''
+Created on Feb 3, 2012
+
+@author: heinz-peterlang
+'''
+
+class ArgumentErrorMsg(object):
+    def __init__(self, function):
+        self.function = function
+        self.error_msg = ""
+     
+    def __call__(self):
+        return "<h1>%s</h1><font color='red'>%s</font><p/><pre>%s</pre>" % \
+             (self.function.__name__, self.error_msg, self.function.__doc__)
+
+class MissingArgumentErrorMsg(ArgumentErrorMsg):
+
+    def __init__(self, function, argument):
+        ArgumentErrorMsg.__init__(self, function)
+        self.error_msg = "Missing argument: '%s'." % (argument) 
+    
+class UnknownArgumentErrorMsg(ArgumentErrorMsg):
+
+    def __init__(self, function, argument):
+        ArgumentErrorMsg.__init__(self, function)
+        self.error_msg = "Unknown argument: '%s'." % (argument)    
