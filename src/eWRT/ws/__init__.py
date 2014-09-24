55f58a96b72e66f3c297bfea768d21a347ce4493
vendi <svitlana.vakulenko@gmail.com>
2014-09-24 11:14:18 +0200

    new web source interface

--- src/eWRT/ws/__init__.py
+++ src/eWRT/ws/__init__.py
@@ -3,3 +3,88 @@
 Web Service access.
 
 """
+
+
+class AbstractWebSource(object):
+
+    ''' a raw Web Source object '''
+
+    NAME = None
+
+    SUPPORTED_PARAMS = None
+
+    # a obligatory mapping, which maps results to a standard dictionary
+
+    # or specifies a WebSourceDocument class.
+
+    MAPPING = {'date': ('valid_from', 'convert_date'),
+
+               'text': ('content', None),
+
+               'title': 'title',
+
+               }
+
+    # MAPPING = WebSourceDocument
+
+    def search_documents(self, search_terms, max_results=None, from_date=None, to_date=None, **kwargs):
+        ''' runs the actual search / calls the webservice / API ... '''
+
+        search_params = self._check_params(kwargs)
+
+        raise NotImplemented
+
+    def pre_search(self, search_params):
+        ''' override this function to perform pre-run tasks '''
+
+    def post_search(self, search_params):
+        ''' override this function to perform post-run tasks '''
+
+    @classmethod
+    def _check_params(cls, params):
+
+        search_params = {}
+
+        for k, v in params.items():
+
+            if k in cls.SUPPORTED_PARAMS:
+
+                search_params[k] = v
+
+        return search_params
+
+    # assert all(param in cls.SUPPORTED_PARAMS for param in params)
+
+
+# class AbstractIterableWebSource(AbstractWebSource):
+
+# DEFAULT_MAX_RESULTS = None  # requires only 1 api access
+#     DEFAULT_START_INDEX = 1
+
+#     def request(self, search_term, num_results=DEFAULT_MAX_RESULTS):
+
+#         response = self.client.execute()
+#         return response
+
+#     def search_documents(self, search_terms, max_results=None, from_date=None, to_date=None, **kwargs):
+#         ''' runs the actual search / calls the webservice / API ... '''
+
+#         search_params = self._check_params(kwargs)
+
+#         for search_term in search_terms:
+
+#             if (max_results > self.DEFAULT_MAX_RESULTS):
+#                 count = self.DEFAULT_MAX_RESULTS
+# number of reguests
+#                 for i in range(self.DEFAULT_START_INDEX, max_results + 1, self.DEFAULT_MAX_RESULTS):
+# detect the last iteration
+#                     if (i + self.DEFAULT_MAX_RESULTS > max_results):
+#                         count = max_results % self.DEFAULT_MAX_RESULTS
+#                     fetched = self.request(search_term, count, index=i)
+
+#             else:
+#                 fetched = self.request(search_term, max_results)
+
+#         raise NotImplemented
+
+#     pass
--- src/eWRT/ws/bing/search.py
+++ src/eWRT/ws/bing/search.py
@@ -5,108 +5,150 @@ Created on Sep 02, 2014
 '''
 
 # Bing API Version 2.0
-# sample URL for web search https://api.datamarket.azure.com/Bing/Search/Web?$format=json&Query=%27Xbox%27&$top=2
+# sample URL for web search
+# https://api.datamarket.azure.com/Bing/Search/Web?$format=json&Query=%27Xbox%27&$top=2
 
 from eWRT.ws.rest import RESTClient
-from pprint import pprint
+from eWRT.ws import AbstractWebSource
+# from pprint import pprint
 
 ROOT_URL = 'https://api.datamarket.azure.com/Bing/Search'
-DEFAULT_FORMAT = 'json'
-DEFAULT_COMMAND = 'Web' # Image, News
-DEFAULT_MAX_RESULTS = 50 # requires only 1 api access
+DEFAULT_COMMAND = 'Web'  # Image, News
 DEFAULT_START_INDEX = 1
 
-class BingSearch(object):
-	# Usage:
-	# pprint(results.next())  # print first
-	# [ pprint(res) for res in results ]  # print all
 
-	def __init__(self, api_key, username, api_url=ROOT_URL):
+class BingSearch(AbstractWebSource):
 
-		assert(api_key)
+    NAME = "Bing Search"
 
-		self.api_key = api_key
-		self.api_url = api_url
-		self.username = username
-		self.client = RESTClient(self.api_url, password=self.api_key, user=self.username, authentification_method='basic')
+    DEFAULT_MAX_RESULTS = 50  # requires only 1 api access
 
-	def request(self, search_term, num_results=DEFAULT_MAX_RESULTS, command = DEFAULT_COMMAND, format=DEFAULT_FORMAT, index=DEFAULT_START_INDEX):
-		''' searches Bing for the given search_term
+    DEFAULT_PARAMS = {'$format': 'json'}
+
+    MAPPING = {'date': ('valid_from', 'convert_date'),
+
+               'text': ('content', None),
+
+               'title': 'Title',
+
+               }
+
+    def __init__(self, api_key, username, api_url=ROOT_URL):
+
+        assert(api_key)
+
+        self.api_key = api_key
+        self.api_url = api_url
+        self.username = username
+        self.client = RESTClient(
+            self.api_url, password=self.api_key, user=self.username, authentification_method='basic')
+
+    def search_documents(self, search_terms, max_results=DEFAULT_MAX_RESULTS, from_date=None, to_date=None, parameters=DEFAULT_PARAMS):
+        ''' runs the actual search / calls the webservice / API ... '''
+        # search_params = self._check_params(kwargs)
+
+        # Web search is by default
+        for search_term in search_terms:
+
+            if (max_results > self.DEFAULT_MAX_RESULTS):
+                mid_results = self.DEFAULT_MAX_RESULTS
+                # number of reguests
+                for i in range(DEFAULT_START_INDEX, max_results + 1, self.DEFAULT_MAX_RESULTS):
+                    # detect the last iteration
+                    if (i + self.DEFAULT_MAX_RESULTS > max_results):
+                        mid_results = max_results % self.DEFAULT_MAX_RESULTS
+                    fetched = self.request(
+                        search_term, mid_results, parameters)
+
+                    for item in fetched['d']['results']:
+                        try:
+                            yield self.convert_item(item)
+                        except Exception as e:  # ported to Python3
+                            print('Error %s occured' % e)
+                            continue
+            else:
+                fetched = self.request(search_term, max_results, parameters)
+
+                # return fetched
+
+                for item in fetched['d']['results']:
+                    try:
+                        yield self.convert_item(item)
+                    except Exception as e:  # ported to Python3
+                        print('Error %s occured' % e)
+                        continue
+
+    def request(self, search_term, max_results=DEFAULT_MAX_RESULTS, parameters=DEFAULT_PARAMS, command=DEFAULT_COMMAND):
+        ''' searches Bing for the given search_term
         '''
-		params = {'Query': search_term,
-	  			  '$format': DEFAULT_FORMAT,
-	  			  '$top': num_results}
+        parameters['Query'] = search_term
+        parameters['$top'] = max_results
 
-		response = self.client.execute(command, query_parameters=params)
-		return response
+        response = self.client.execute(command, query_parameters=parameters)
+        # print(response)
+        return response
 
-	def search(self, search_terms, num_results=DEFAULT_MAX_RESULTS, command = DEFAULT_COMMAND, format=DEFAULT_FORMAT, index=DEFAULT_START_INDEX):
-		# Web search is by default
-		for search_term in search_terms:
+    @classmethod
+    def convert_item(cls, item):
+        ''' applies a mapping to convert the result to the required format
+        '''
 
-			if (num_results > DEFAULT_MAX_RESULTS):
-				count = DEFAULT_MAX_RESULTS
-				for i in range(DEFAULT_START_INDEX, num_results+1, DEFAULT_MAX_RESULTS):  # number of reguests
-					if (i + DEFAULT_MAX_RESULTS > num_results):  # detect the last iteration
-						count = num_results % DEFAULT_MAX_RESULTS
-					fetched = self.request(search_term, count, index=i)
+        result = {'url': item['Url'],
+                  'title': item['Title'],
+                  }
 
-					for item in fetched['d']['results']:
-					    try:
-					        yield self.convert_item(item)
-					    except Exception as e:  # ported to Python3
-					        print('Error %s occured' % e)
-					        continue
-			else:
-				fetched = self.request(search_term, num_results)
-
-				for item in fetched['d']['results']:
-				    try:
-				        yield self.convert_item(item)
-				    except Exception as e:  # ported to Python3
-				        print('Error %s occured' % e)
-				        continue
-
-	@classmethod
-	def convert_item(cls, item):
-	    ''' applies a mapping to convert the result to the required format
-	    '''
-
-	    result = {'url':item['Url'],
-	    		  'title': item['Title'],
-	    		  }
-
-	    return result
+        return result
 
 
 class TestBingSearch(object):
-	# tested only Web command
+    # tested only Web command
 
-	username = 'svitlana.vakulenko@gmail.com'
-	my_account_key = 'kieVYyuW1uwvUnVo3b1+vXWtim6TuxFiYkSpPtloUhI'
+    username = 'svitlana.vakulenko@gmail.com'
+    my_acmid_results_key = 'kieVYyuW1uwvUnVo3b1+vXWtim6TuxFiYkSpPtloUhI'
 
-	search_terms = ["'modul'", "'university'"]
+    search_terms = ["'modul'", "'university'"]
 
-	# test default api call (limit = DEFAULT_MAX_RESULTS)
-	def test_default(self):
-		bs = BingSearch(self.my_account_key, self.username)
-		results = bs.search(self.search_terms)
+    # test default api call (max_results = DEFAULT_MAX_RESULTS)
+    def test_default(self):
+        bs = BingSearch(self.my_acmid_results_key, self.username)
 
-		# assert the correct number of the results
-		assert len(list(results)) == DEFAULT_MAX_RESULTS * len(self.search_terms)
+        results = bs.search_documents(self.search_terms)
 
-	def test_smaller_limit(self):
-		limit = 4
-		bs = BingSearch(self.my_account_key, self.username)
-		results = bs.search(self.search_terms, limit)
+        # assert the correct number of the results
+        assert len(list(results)) == bs.DEFAULT_MAX_RESULTS * \
+            len(self.search_terms)
 
-		# assert the correct number of the results
-		assert len(list(results)) == limit * len(self.search_terms)
+    # test api call with additional parameters
+    def test_params(self):
+        bs = BingSearch(self.my_acmid_results_key, self.username)
 
-	def test_larger_limit(self):
-		limit = 70
-		bs = BingSearch(self.my_account_key, self.username)
-		results = bs.search(self.search_terms, limit)
+        params = {'$format': 'json'}
 
-		# assert the correct number of the results
-		assert len(list(results)) == limit * len(self.search_terms)
+        results = bs.search_documents(
+            self.search_terms, parameters=params)
+
+        # print(results)
+        # assert the correct number of the results
+        assert len(list(results)) == bs.DEFAULT_MAX_RESULTS * \
+            len(self.search_terms)
+        # [pprint(res) for res in results]
+
+    def test_smaller_max_results(self):
+        max_results = 4
+        bs = BingSearch(self.my_acmid_results_key, self.username)
+        results = bs.search_documents(self.search_terms, max_results)
+
+    # assert the correct number of the results
+        assert len(list(results)) == max_results * len(self.search_terms)
+
+    def test_larger_max_results(self):
+        max_results = 70
+        bs = BingSearch(self.my_acmid_results_key, self.username)
+        results = bs.search_documents(self.search_terms, max_results)
+
+    # assert the correct number of the results
+        assert len(list(results)) == max_results * len(self.search_terms)
+
+if __name__ == '__main__':
+    test = TestBingSearch()
+    test.test_params()
