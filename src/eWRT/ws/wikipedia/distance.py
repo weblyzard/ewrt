c36d5e824a16c06d315968cca82ea7343d685158
Max Goebel <mcgoebel@gmail.com>
2017-11-29 23:09:16 +0100

    fix: commented out defunct unittests

--- src/eWRT/ws/wikipedia/distance.py
+++ src/eWRT/ws/wikipedia/distance.py
@@ -1,76 +1,76 @@
-#!/usr/bin/env python
-"""
-   @package eWRT.ws.wikipedia.distance
-   wikipedia "click" distance between concepts
-"""
-
-# -----------------------------------------------------------------------------------
-# (C)opyrights 2010 by Albert Weichselbraun <albert@weichselbraun.net>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# -----------------------------------------------------------------------------------
-
-__author__   = "albert"
-__revision__ = "$Revision: 1 $"
-
-from eWRT.access.db import PostgresqlDb
-from eWRT.config import DATABASE_CONNECTION
-
-class WikiDistance(object):
-    
-    escape_terms = staticmethod( lambda tt:", ".join( ["'%s'" % t.replace("'", "''") for t in tt]) ) 
-    
-    def __init__(self):
-        """ open the database connection """
-        dbParam = DATABASE_CONNECTION['wikipedia'].copy()
-        dbParam['connect'] = False
-        self.db = PostgresqlDb( **dbParam)
-
-    def isSibling(self, t1, t2):
-        """ determines whether the given concepts are siblings
-            @param[in] t1   first term
-            @param[in] t2   second term
-            @return True if both terms are siblings
-        """
-        tt = WikiDistance.escape_terms( (t1,t2) ) 
-        with self.db as c:
-            q="SELECT * FROM vw_siblings WHERE dname IN (%s) AND sname IN (%s)" % (tt, tt)
-            cnt = len(c.query(q))
-        return cnt > 0 
-
-    def isSameAs(self, t1, t2):
-        """ determines whether the given terms refer to the same concept
-            @param[in] t1   first term
-            @param[in] t2   second term
-            @return True if both terms refer to the same concept
-        """
-        tt = WikiDistance.escape_terms( (t1,t2) )
-        with self.db as c:
-            q="SELECT * FROM vw_sameas WHERE dname IN (%s) AND sname IN (%s)" % (tt, tt)
-            cnt = len(c.query(q))
-        return cnt > 0 
-
-
-def p_isSibling( concepts ):
-    """ 
-        @param[in] concepts a tuple containing the two concepts to check
-
-        @remarks
-        helper function for the testMultiProcessing test
-    """
-    assert len(concepts) == 2
-    w = WikiDistance()
-    return w.isSameAs( *concepts ), w.isSibling( *concepts )
+# #!/usr/bin/env python
+# """
+#    @package eWRT.ws.wikipedia.distance
+#    wikipedia "click" distance between concepts
+# """
+# 
+# # -----------------------------------------------------------------------------------
+# # (C)opyrights 2010 by Albert Weichselbraun <albert@weichselbraun.net>
+# #
+# #  This program is free software: you can redistribute it and/or modify
+# #  it under the terms of the GNU General Public License as published by
+# #  the Free Software Foundation, either version 3 of the License, or
+# #  (at your option) any later version.
+# #
+# #  This program is distributed in the hope that it will be useful,
+# #  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# #  GNU General Public License for more details.
+# #
+# #  You should have received a copy of the GNU General Public License
+# #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# #
+# # -----------------------------------------------------------------------------------
+# 
+# __author__   = "albert"
+# __revision__ = "$Revision: 1 $"
+# 
+# from eWRT.access.db import PostgresqlDb
+# from eWRT.config import DATABASE_CONNECTION
+# 
+# class WikiDistance(object):
+#     
+#     escape_terms = staticmethod( lambda tt:", ".join( ["'%s'" % t.replace("'", "''") for t in tt]) ) 
+#     
+#     def __init__(self):
+#         """ open the database connection """
+#         dbParam = DATABASE_CONNECTION['wikipedia'].copy()
+#         dbParam['connect'] = False
+#         self.db = PostgresqlDb( **dbParam)
+# 
+#     def isSibling(self, t1, t2):
+#         """ determines whether the given concepts are siblings
+#             @param[in] t1   first term
+#             @param[in] t2   second term
+#             @return True if both terms are siblings
+#         """
+#         tt = WikiDistance.escape_terms( (t1,t2) ) 
+#         with self.db as c:
+#             q="SELECT * FROM vw_siblings WHERE dname IN (%s) AND sname IN (%s)" % (tt, tt)
+#             cnt = len(c.query(q))
+#         return cnt > 0 
+# 
+#     def isSameAs(self, t1, t2):
+#         """ determines whether the given terms refer to the same concept
+#             @param[in] t1   first term
+#             @param[in] t2   second term
+#             @return True if both terms refer to the same concept
+#         """
+#         tt = WikiDistance.escape_terms( (t1,t2) )
+#         with self.db as c:
+#             q="SELECT * FROM vw_sameas WHERE dname IN (%s) AND sname IN (%s)" % (tt, tt)
+#             cnt = len(c.query(q))
+#         return cnt > 0 
+# 
+# 
+# def p_isSibling( concepts ):
+#     """ 
+#         @param[in] concepts a tuple containing the two concepts to check
+# 
+#         @remarks
+#         helper function for the testMultiProcessing test
+#     """
+#     assert len(concepts) == 2
+#     w = WikiDistance()
+#     return w.isSameAs( *concepts ), w.isSibling( *concepts )
 
--- tests/ws/test_delicious.py
+++ tests/ws/test_delicious.py
@@ -1,82 +1,82 @@
-#!/usr/bin/env python
-
-""" unittests covering the del.icio.us functions provided by eWRT """
-
-# (C)opyrights 2008 by Albert Weichselbraun <albert@weichselbraun.net>
+# #!/usr/bin/env python
 # 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# """ unittests covering the del.icio.us functions provided by eWRT """
 # 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# # (C)opyrights 2008 by Albert Weichselbraun <albert@weichselbraun.net>
+# # 
+# # This program is free software: you can redistribute it and/or modify
+# # it under the terms of the GNU General Public License as published by
+# # the Free Software Foundation, either version 3 of the License, or
+# # (at your option) any later version.
+# # 
+# # This program is distributed in the hope that it will be useful,
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# # GNU General Public License for more details.
+# # 
+# # You should have received a copy of the GNU General Public License
+# # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import unittest
-
-from nose.plugins.attrib import attr
-
-from eWRT.ws.delicious import Delicious
-
-
-DELICIOUS_TEST_URLS = ( 'http://www.iaeste.at', 'http://www.wu-wien.ac.at', 
-                        'http://www.heise.de', 'http://www.kurier.at', 
-                        'http://news.bbc.co.uk', )
-
-DELICIOUS_TEST_TAGS = [("linux",), ("information",), ("information", "retrieval"),
-                       ("linux", "debian"),("algore",)]
-
-RELATED_TAGS_DELICIOUS_PAGE = './data/delicious_climate_related_tags.html'
-    
-class TestDelicious(unittest.TestCase):
-
-    def test_url_info(self):
-        for url in DELICIOUS_TEST_URLS:
-            print '%s has %s counts '% (url, Delicious.delicious_info_retrieve(url))
-
-    def test_tag_info(self):
-        print '### Testing tag_info ###'
-        for tags in DELICIOUS_TEST_TAGS:
-            print '%s has %s counts ' % (tags, Delicious.getTagInfo( tags ))
-
-    
-    def test_related_tags(self):
-        print '### Testing related_tags ###'
-        for tags in DELICIOUS_TEST_TAGS:
-            print '%s has related tags: %s' % (tags, Delicious.getRelatedTags( tags ))
-    
-    def test_tag_splitting(self):
-        """ verifies the correct handling of tags containing spaces
-            i.e. (t1, t2, t3) == (t1, "t2 t3") """
-        d = Delicious._parse_tag_url
-        print d( ("debian linux") )
-        assert d( ("debian", "linux" )) == d( ("debian linux", ) )
-        assert d( ("t1", "t2", "t3") ) == d( ("t1", "t2 t3") )
-
-    def test_ngram_related_tags(self):
-        """ tests support for related tags for n-grams """
-        assert len( Delicious().getRelatedTags( ("climate", "change") ) ) > 0
-
-        content = open( TestDelicious.RELATED_TAGS_DELICIOUS_PAGE ).read()
-        related_tags = Delicious._getNGramRelatedTags( content )
-
-        assert 'global' in related_tags
-        assert 'evidence' in related_tags
-        assert 'vegetarian' in related_tags
-        assert 'sustainability' in related_tags
-
-        assert 'linux' not in related_tags
-
-    @attr("remote")
-    def test_critical_tag_names(self):
-        """ tests tag names which contain slashes, quotes, etc """
-        assert Delicious.getTagInfo( ("consequence/frequency matrix", ) ) != None
-        assert Delicious.getTagInfo( ("it's", )) != None
-
-if __name__ == '__main__':
-    unittest.main()
+# import unittest
+# 
+# from nose.plugins.attrib import attr
+# 
+# from eWRT.ws.delicious import Delicious
+# 
+# 
+# DELICIOUS_TEST_URLS = ( 'http://www.iaeste.at', 'http://www.wu-wien.ac.at', 
+#                         'http://www.heise.de', 'http://www.kurier.at', 
+#                         'http://news.bbc.co.uk', )
+# 
+# DELICIOUS_TEST_TAGS = [("linux",), ("information",), ("information", "retrieval"),
+#                        ("linux", "debian"),("algore",)]
+# 
+# RELATED_TAGS_DELICIOUS_PAGE = './data/delicious_climate_related_tags.html'
+#     
+# class TestDelicious(unittest.TestCase):
+# 
+#     def test_url_info(self):
+#         for url in DELICIOUS_TEST_URLS:
+#             print '%s has %s counts '% (url, Delicious.delicious_info_retrieve(url))
+# 
+#     def test_tag_info(self):
+#         print '### Testing tag_info ###'
+#         for tags in DELICIOUS_TEST_TAGS:
+#             print '%s has %s counts ' % (tags, Delicious.getTagInfo( tags ))
+# 
+#     
+#     def test_related_tags(self):
+#         print '### Testing related_tags ###'
+#         for tags in DELICIOUS_TEST_TAGS:
+#             print '%s has related tags: %s' % (tags, Delicious.getRelatedTags( tags ))
+#     
+#     def test_tag_splitting(self):
+#         """ verifies the correct handling of tags containing spaces
+#             i.e. (t1, t2, t3) == (t1, "t2 t3") """
+#         d = Delicious._parse_tag_url
+#         print d( ("debian linux") )
+#         assert d( ("debian", "linux" )) == d( ("debian linux", ) )
+#         assert d( ("t1", "t2", "t3") ) == d( ("t1", "t2 t3") )
+# 
+#     def test_ngram_related_tags(self):
+#         """ tests support for related tags for n-grams """
+#         assert len( Delicious().getRelatedTags( ("climate", "change") ) ) > 0
+# 
+#         content = open( TestDelicious.RELATED_TAGS_DELICIOUS_PAGE ).read()
+#         related_tags = Delicious._getNGramRelatedTags( content )
+# 
+#         assert 'global' in related_tags
+#         assert 'evidence' in related_tags
+#         assert 'vegetarian' in related_tags
+#         assert 'sustainability' in related_tags
+# 
+#         assert 'linux' not in related_tags
+# 
+#     @attr("remote")
+#     def test_critical_tag_names(self):
+#         """ tests tag names which contain slashes, quotes, etc """
+#         assert Delicious.getTagInfo( ("consequence/frequency matrix", ) ) != None
+#         assert Delicious.getTagInfo( ("it's", )) != None
+# 
+# if __name__ == '__main__':
+#     unittest.main()
