820d4c90dae7706709fcc1c952e98d50dcfe4ca5
Max Goebel <mcgoebel@gmail.com>
2018-03-24 22:35:44 +0100

    chg: upped version, trove classifier changed to python 3.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ###########################################
     # Metadata
     name="eWRT",
-    version="0.9.5-dev",
+    version="1.0-dev",
     description='eWRT',
     author='Albert Weichselbraun, Heinz Lang, Gerhard Wohlgenannt, Johannes Duong, Norman Süsstrunk, Daniel Streiff',
     author_email='albert@weblyzard.com, lang@weblyzard.com, wohlg@ai.wu.ac.at, johannes.duong@wu.ac.at, norman.suestrunk@htwchur.ch, daniel.streiff@htwchur.ch',
@@ -20,13 +20,11 @@ setup(
     install_requires=['redis',
                       'oauth2client==2.2.0',  # YT
                       'google-api-python-client==1.4.0',  # YT,
-                      'six',
-                      #                         'python-oauth2' #linkedin
+                      'six'
                       ],
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
     ],
 
 
