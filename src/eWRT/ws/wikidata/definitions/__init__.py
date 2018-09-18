cf4b7f44071724c142851dbac2fd297a02af6141
Max Goebel <mcgoebel@gmail.com>
2018-09-18 16:51:27 +0200

    chg: use absolute imports

--- src/eWRT/ws/wikidata/definitions/__init__.py
+++ src/eWRT/ws/wikidata/definitions/__init__.py
@@ -1,9 +1,9 @@
-from property_definitions import (image_attributes,
-                                  local_attributes,
-                                  person_properties,
-                                  location_properties,
-                                  organization_properties,
-                                  ENTITY_TYPE_DEFINITIONS,
-                                  GENERIC_PROPERTIES)
+from eWRT.ws.wikidata.definitions.property_definitions import (image_attributes,
+                                                               local_attributes,
+                                                               person_properties,
+                                                               location_properties,
+                                                               organization_properties,
+                                                               ENTITY_TYPE_DEFINITIONS,
+                                                               GENERIC_PROPERTIES)
 
-from countries_iso_codes import COUNTRY_ISO2_CODES_DICT
+from eWRT.ws.wikidata.definitions.countries_iso_codes import COUNTRY_ISO2_CODES_DICT
