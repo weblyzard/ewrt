80111d082c7430a68c6f3e05f68e008b35e8108b
Max Goebel <mcgoebel@gmail.com>
2018-09-18 16:26:44 +0200

    new: 3rd parties, chg: upped version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ###########################################
     # Metadata
     name="eWRT",
-    version="0.9.7.20180817-dev",
+    version="0.9.7.20180918-dev",
     description='eWRT',
     author='Albert Weichselbraun, Heinz Lang, Gerhard Wohlgenannt, Johannes Duong, Norman Süsstrunk, Daniel Streiff',
     author_email='albert@weblyzard.com, lang@weblyzard.com, wohlg@ai.wu.ac.at, johannes.duong@wu.ac.at, norman.suestrunk@htwchur.ch, daniel.streiff@htwchur.ch',
@@ -22,7 +22,9 @@ setup(
                       'oauth2client==2.2.0',  # YT
                       'google-api-python-client==1.4.0',  # YT,
                       'six',
-                      'gdata'
+                      'gdata',
+                      'wikipedia',
+                      'pywikibot'
                       #                         'python-oauth2' #linkedin
                       ],
     classifiers=[
