56e530182f3ae056086cb58d3eb8ead330c345bf
jakob.steixner <jakob.m.steixner@gmail.com>
2018-10-31 11:25:48 +0100

    fix: move tests.

---
+++ tests/ws/wikidata/test_get_info_by_id.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on October 19, 2018
+
+@author: jakob <jakob.steixner@modul.ac.at>
+'''
+# from unittest import TestCase
+#
+#
+# class TestName_or_id(TestCase):
+#     def test_name_or_id(self):
+#         self.fail()
