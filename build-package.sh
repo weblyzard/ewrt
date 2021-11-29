8d38b238647b94c7e93f59aad89395011a79378e
Reinhard Fischer <reinhard.fischer@wu.ac.at>
2021-11-29 13:04:58 +0000

    Update build-package.sh add twine upload to nexus

--- build-package.sh
+++ build-package.sh
@@ -11,4 +11,6 @@ VERSION=$(/usr/bin/python3 -W ignore ./setup.py --version | awk 'NR==1{print $1}
 # this line ensures that this released version cannot be overwritten
 cd /var/www/repo3/ && ls -1 | grep $PACKAGE | grep -v "$PACKAGE.*-dev.*" | grep $VERSION | xargs chmod a-w
 
+twine upload /var/www/repo3/$PACKAGE-$VERSION.tar.gz  --repository-url 'https://nexus.semanticlab.net/repository/python3/' -u 'python' -p '!33Autumn33!'
+
 echo ":: Current version of ${PACKAGE} is ${VERSION}."
